dd536d52c4c4b4f802fb4bde77ca7e43b2068cc9
FelixAbrahamsson <felixab@kth.se>
2021-09-08 21:01:38 +0200

    refactor: remove unused methods from functional base

--- kmm/functional_base.py
+++ kmm/functional_base.py
@@ -13,26 +13,3 @@ class FunctionalBase(BaseModel):
         new_dict = self.dict()
         new_dict.update(**kwargs)
         return type(self)(**new_dict)
-
-    @classmethod
-    def setattr(cls, name, value):
-        if hasattr(cls, name):
-            raise ValueError(f"Attribute {name} already exists")
-        setattr(cls, name, value)
-        return value
-
-    @classmethod
-    def method(cls, fn):
-        return cls.setattr(fn.__name__, fn)
-
-    @classmethod
-    def property(cls, fn):
-        return cls.setattr(fn.__name__, property(fn))
-
-    @classmethod
-    def staticmethod(cls, fn):
-        return cls.setattr(fn.__name__, staticmethod(fn))
-
-    @classmethod
-    def classmethod(cls, fn):
-        return cls.setattr(fn.__name__, classmethod(fn))
