6016a573ea85af1ac360fad11573f32fc4d06b2f
FelixAbrahamsson <felixab@kth.se>
2021-09-08 11:46:38 +0200

    refactor: positions object instead of just dataframe

--- kmm/__init__.py
+++ kmm/__init__.py
@@ -1,3 +1,4 @@
+from kmm.functional_base import FunctionalBase
 from kmm.kmm import kmm
 from kmm.kmm2 import kmm2
 from kmm.read_raw import read_raw
@@ -5,6 +6,6 @@ from kmm.geodetic import geodetic
 from kmm.camera_positions import camera_positions
 from kmm.sync_frame_index import sync_frame_index
 from kmm.position_adjustment import PositionAdjustment
-from kmm.read_sync_adjust import read_sync_adjust
+from kmm.positions import Positions
 
 WIRE_CAMERA_ADJUSTMENT = 1
--- kmm/read_raw.py
+++ kmm/read_raw.py
@@ -6,6 +6,20 @@ import kmm
 
 @validate_arguments
 def read_raw(path: Path):
+    """
+    Used to load a kmm or kmm2 file as a pandas DataFrame.
+
+    Example:
+
+    .. code-block:: python
+
+        from pathlib import Path
+        import kmm
+
+        path = Path("...")
+        df = kmm.read(path)
+
+    """
     if path.suffix == ".kmm":
         df = kmm.kmm(path)
     elif path.suffix == ".kmm2":
