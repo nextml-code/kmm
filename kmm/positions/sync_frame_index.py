6a292b2b39cb67aa5a8872b7df6bbe15bd30fdb9
FelixAbrahamsson <felixab@kth.se>
2021-09-12 17:08:05 +0200

    improve: validate meter increments to be somewhat consistent

--- kmm/positions/sync_frame_index.py
+++ kmm/positions/sync_frame_index.py
@@ -9,6 +9,7 @@ from kmm.header.header import Header
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
 def sync_frame_index(positions: Positions, header: Header):
 
+    validate_meter_increments(positions)
     frame_index = (
         (
             positions.dataframe["centimeter"].values
@@ -29,6 +30,18 @@ def sync_frame_index(positions: Positions, header: Header):
     return positions.replace(
         dataframe=positions.dataframe.assign(frame_index=frame_index)
     )
+
+
+def validate_meter_increments(positions):
+    for (track_section, kilometer), group in (
+        positions.dataframe
+        .groupby(["track_section", "kilometer"])
+    ):
+        diffs = np.sign(group["meter"].values[1:] - group["meter"].values[:-1])
+        if len(diffs) >= 10 and (diffs > 0).mean() < 0.9 and (diffs < 0).mean() < 0.9:
+            raise ValueError(
+                f"Inconsistent directions at track_section {track_section}, kilometer {kilometer}."
+            )
 
 
 def test_sync_frame_index_kmm():
