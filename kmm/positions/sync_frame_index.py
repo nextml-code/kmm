d9810ff40f1bc2ffe36deafd7455a61e007fa5dd
FelixAbrahamsson <felixab@kth.se>
2021-10-05 13:18:06 +0200

    test: better frame index sync tests

--- kmm/positions/sync_frame_index.py
+++ kmm/positions/sync_frame_index.py
@@ -48,23 +48,49 @@ def validate_meter_increments(positions):
             )
 
 
-def test_sync_frame_index_kmm():
+def test_sync_frame_index_ascending_B_kmm():
     from kmm import Header
 
-    positions = Positions.from_path("tests/ascending_B.kmm")
-    header = Header.from_path("tests/ascending_B.hdr")
-    assert (
-        sync_frame_index(positions, header, PositionAdjustment.WIRE_CAMERA)
-        .dataframe["frame_index"].iloc[0] == 659
-    )
+    synced = sync_frame_index(
+        Positions.from_path("tests/ascending_B.kmm"),
+        Header.from_path("tests/ascending_B.hdr"),
+        PositionAdjustment.WIRE_CAMERA,
+    ).dataframe
+    assert synced[synced["frame_index"] == 675]["kilometer"].values[0] == 292
+    assert synced[synced["frame_index"] == 675]["meter"].values[0] == 737 + 8
 
 
-def test_sync_frame_index_kmm2():
+def test_sync_frame_index_ascending_B_kmm2():
     from kmm import Header
 
-    positions = Positions.from_path("tests/ascending_B.kmm2")
-    header = Header.from_path("tests/ascending_B.hdr")
-    assert (
-        sync_frame_index(positions, header, PositionAdjustment.WIRE_CAMERA)
-        .dataframe["frame_index"].iloc[0] == -808
-    )
+    synced = sync_frame_index(
+        Positions.from_path("tests/ascending_B.kmm2"),
+        Header.from_path("tests/ascending_B.hdr"),
+        PositionAdjustment.WIRE_CAMERA,
+    ).dataframe
+    assert synced[synced["frame_index"] == 5]["kilometer"].values[0] == 292
+    assert synced[synced["frame_index"] == 5]["meter"].values[0] == 802 + 8
+
+
+def test_sync_frame_index_ascending_A():
+    from kmm import Header
+
+    synced = sync_frame_index(
+        Positions.from_path("tests/ascending_A.kmm2"),
+        Header.from_path("tests/ascending_A.hdr"),
+        PositionAdjustment.WIRE_CAMERA,
+    ).dataframe
+    assert synced[synced["frame_index"] == 8]["kilometer"].values[0] == 534
+    assert synced[synced["frame_index"] == 8]["meter"].values[0] == 336 - 8
+
+
+def test_sync_frame_index_descending_A():
+    from kmm import Header
+
+    synced = sync_frame_index(
+        Positions.from_path("tests/descending_A.kmm2"),
+        Header.from_path("tests/descending_A.hdr"),
+        PositionAdjustment.WIRE_CAMERA,
+    ).dataframe
+    assert synced[synced["frame_index"] == 1000]["kilometer"].values[0] == 57
+    assert synced[synced["frame_index"] == 1000]["meter"].values[0] == 2 + 8
