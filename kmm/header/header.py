a82769925574beb63360cb51499ec537ee9e51df
FelixAbrahamsson <felixabrahamsson1@gmail.com>
2023-11-10 11:03:46 +0100

    improve: attempt to patch broken xml

--- kmm/header/header.py
+++ kmm/header/header.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from xml.etree import ElementTree
+
 from pydantic import validate_arguments
 
 import kmm
@@ -18,8 +19,8 @@ class Header(kmm.FunctionalBase):
         """
         try:
             tree = ElementTree.parse(path)
-        except ElementTree.ParseError as e:
-            raise ValueError("Unable to parse header file, invalid XML.") from e
+        except ElementTree.ParseError:
+            tree = attempt_to_patch_xml(path)
 
         position, sync = kmm.header.position_sync(tree)
         return Header(
@@ -27,6 +28,33 @@ class Header(kmm.FunctionalBase):
             sync=sync,
             car_direction=kmm.header.car_direction(tree),
         )
+
+
+def attempt_to_patch_xml(path: Path):
+    """
+    Attempts to patch a broken header file by adding missing end tags.
+    """
+    lines = path.read_text().splitlines()
+    start_tags = []
+    for line in lines:
+        if line.startswith("</") and line.endswith(">"):
+            try:
+                index = max(
+                    i
+                    for i, start_tag in enumerate(start_tags)
+                    if start_tag == line[2:-1]
+                )
+            except ValueError:
+                raise ElementTree.ParseError(f"Missing start tag for {line}")
+            start_tags.pop(index)
+        elif line.startswith("<") and line.endswith(">") and not line.startswith("<?"):
+            start_tags.append(line[1:-1])
+    for start_tag in start_tags[::-1]:
+        lines.append(f"</{start_tag}>")
+    try:
+        return ElementTree.ElementTree(ElementTree.fromstring("\n".join(lines)))
+    except ElementTree.ParseError as e:
+        raise ValueError("Unable to parse header file, invalid XML.") from e
 
 
 def test_header():
